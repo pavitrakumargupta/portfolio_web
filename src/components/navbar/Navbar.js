1d452cea17a73d3fcf91b955b2b9c15dd4817d64
pavitrakumargupta <pavitragupta021@gmail.com>
2023-02-09 23:56:01 +0530

    navbar fully done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,13 +6,28 @@ import { CgMenu, CgClose } from "react-icons/cg";
 const Navbar = () => {
   const [menuIcon, setMenuIcon] = useState(false);
   const services=["Home","About","Skills","Projects"]
+
+  const fieldStyle={
+    borderBottom:"5px solid",
+    fontSize:"25px",
+    marginTop:"-5px"
+  }
+  const [fieldsnameStyle,setfieldnameStyle]=useState({
+    Home: fieldStyle,
+  })
+  const handleFieldstyle=(event)=>{
+    const name=event.target.name;
+    console.log(name)
+    setfieldnameStyle(()=>{return{[name]:fieldStyle}})
+  } 
+
   return (
     <nav className={menuIcon ? 'nav active' : 'nav'}> 
       <img src={Logo} alt="" />
       <div  className= 'pages'>
         {services.map(key=>{
           let url="#"+key
-          return(<a href={url}><button onClick={() => setMenuIcon(false)} name={key}>{key}</button></a>)
+          return(<a href={url}><button style={fieldsnameStyle[key]} onClick={(event) => {setMenuIcon(false); handleFieldstyle(event)}} name={key}>{key}</button></a>)
         }
         )}
       </div>  
