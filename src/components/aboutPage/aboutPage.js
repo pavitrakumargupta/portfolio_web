b18ad0d1d36e45246f13a77ddfa6988832e3b0fd
pavitrakumargupta <pavitragupta021@gmail.com>
2023-02-11 04:15:16 +0530

    done Portfolio website

--- src/components/aboutPage/aboutPage.js
+++ src/components/aboutPage/aboutPage.js
@@ -10,10 +10,10 @@ const aboutPage = () => {
         <div className='field'>
             <div className='textfield'>
             <img src={profile} alt="" />
-             <p>Hi, I'm [ Pavitra Kumar Gupta ], a full-stack developer with 1 years of experience building dynamic, user-friendly web applications. I have a passion for coding and love to take complex problems and turn them into intuitive and elegant solutions.
-              With a strong background in [React.js, Node.js, MongoDB, Javascript, HTML ,CSS ], I am proficient in both front-end and back-end development. I am comfortable working with a variety of technologies, including [Bootstrap, TailWind, Git, REST APIs, AWS, Firebase, Heroku].
+             <p>Hi, I'm  Pavitra Kumar Gupta , a full-stack developer with 1 years of experience building dynamic, user-friendly web applications. I have a passion for coding and love to take complex problems and turn them into intuitive and elegant solutions.
+              With a strong background in React.js, Node.js, MongoDB, Javascript, HTML ,CSS , I am proficient in both front-end and back-end development. I am comfortable working with a variety of technologies, including Bootstrap, TailWind, Git, REST APIs, AWS, Firebase, Heroku, Netlify.
               I am a team player, always looking for ways to collaborate and learn from others, but also able to work independently and take ownership of projects from start to finish.
-              When I am not coding, I enjoy [Your Hobbies/Interests].
+              When I am not coding, I enjoy  reading books music and specially workout. <br />
               Feel free to take a look at my portfolio and reach out if you'd like to work together on a project.</p>
               
             </div>
--- src/components/services/services.js
+++ src/components/services/services.js
@@ -10,23 +10,52 @@ import {
 } from 'mdb-react-ui-kit';
 const services = () => {
   const projects=[1,2,3,4,5]
+  const project=[
+    {
+      ProjectTittle:"Blog App",
+      definition:"Blog app where you can Post your blogs and have your own account while surfing Just create account and start surfing",
+      ImageLink:"https://user-images.githubusercontent.com/88044814/218199731-a2966f2d-4edd-4ee3-8194-4738096b1c6a.png",
+      CodeLink:"https://github.com/pavitrakumargupta/Blog-Posting--App",
+      Link:"https://taupe-cactus-098771.netlify.app/"
+    },
+    {
+      ProjectTittle:"Tin Dog",
+      definition:"It is a static dummy website where u can check out a pet dog at diffrent prices",
+      ImageLink:"https://user-images.githubusercontent.com/88044814/218204186-6363c940-6dd1-4e4c-8ef1-185f2ccd6a0c.png",
+      CodeLink:"https://github.com/pavitrakumargupta/Tin_dog",
+      Link:"https://pavitrakumargupta.github.io/Tin_dog/tindog/index.html"
+    },{
+      ProjectTittle:"Simon_Game_Challenge",
+      definition:"game of remembring the pattern it is just small game where will system automatically generate diifrent pattern you just have to remmber it follow it ",
+      ImageLink:"https://user-images.githubusercontent.com/88044814/218207214-7daadd30-8c96-4df2-b51c-bcbcc49dcecd.png",
+      CodeLink:"https://github.com/pavitrakumargupta/Simon_Game_Challenge",
+      Link:"https://pavitrakumargupta.github.io//Simon_Game_Challenge/simon-game/index.html"
+    },{
+      ProjectTittle:"Simon_Game_Challenge",
+      definition:"Website to plays different kinds of musical instruments that ca entertain with sounds and can with diffrent sounds",
+      ImageLink:"https://user-images.githubusercontent.com/88044814/218208097-b2fc5be8-d8ff-4dec-9426-0c4beebd50e4.png",
+      CodeLink:"https://github.com/pavitrakumargupta/Drum_Kit_Sounds",
+      Link:"https://pavitrakumargupta.github.io//Drum_Kit_Sounds/drum-kit/index.html"
+    }
+  ]
   return (
     <div id='Projects' className='service'>
          <h1>Projects</h1>
          <div className='Projects'>
-          {projects.map(key=>(
+          {project.map(key=>(
               <MDBCard className='project'>
-                <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/111.webp' fluid alt='...' />
+                <MDBCardImage src={key.ImageLink} fluid alt='...' />
               <MDBCardBody className='card_body'>
-                <MDBCardTitle>Card title</MDBCardTitle>
-                <MDBCardText>
-                  Some quick example text to build on the card title and make up the bulk of the card's content.
-                </MDBCardText>
-                <MDBBtn href='#'>Button</MDBBtn>
+                <MDBCardTitle>{key.ProjectTittle}</MDBCardTitle>
+                <MDBCardText>{key.definition}</MDBCardText>
+                <div style={{display:"flex",justifyContent:"space-around"}}>
+                  <MDBBtn href={key.CodeLink}>Code Link ↗</MDBBtn>
+                  <MDBBtn href={key.Link}>Web Link ↗</MDBBtn>
+                </div>
               </MDBCardBody>
             </MDBCard>
           ))}
-         </div>
+         </div> 
     </div>
   )
 }
